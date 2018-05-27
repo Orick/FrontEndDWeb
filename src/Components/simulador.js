559650eaaba89c1f1985a1748d713e187ac9b301
Eduardo Antonio Peña Dávila <eduardo.pea45@gmail.com>
2018-05-27 10:59:02 -0400

    Simulador update

--- src/Components/simulador.js
+++ src/Components/simulador.js
@@ -20,7 +20,8 @@ class Simulador extends Component {
             iditem23: 0,
             iditem24: 0,
             iditem25: 0,
-            iditem26: 0
+            iditem26: 0,
+            itemdata: null
         };
 
         this.renderMenuItem = this.renderMenuItem.bind(this);
@@ -28,9 +29,9 @@ class Simulador extends Component {
         this.getItems = this.getItems.bind(this);
     }
 
-    renderMenuItem(champ, i) {
+    renderMenuItem(item, i) {
         return (
-            <MenuItem eventKey={champ[0]}>{champ[1]}</MenuItem>
+            <MenuItem eventKey={item[0]}>{item[1]}</MenuItem>
         );
     }
 
@@ -40,25 +41,100 @@ class Simulador extends Component {
     }
 
     getItems(){
-        const champs = [[50, 'Anie'], [14 ,'Warwick'], [25 ,'Urgot'], [10 ,'Miss Fortune'], [85 ,'Rexai']];
-        return (champs);
+        fetch('http://localhost:8080/items/data/basic')
+        .then(response => response.json())
+        .then(result => {
+            this.setState({
+                itemdata: result.data
+            });
+        })
+        .catch( error => {
+            console.log("fectch error : ", error);
+        });
+        
+        return this.state.itemdata;
     }
 
     render() {
         const {idchamp1, idchamp2, iditem11, iditem12, iditem13, iditem14, iditem15, iditem16, iditem21, iditem22, iditem23, iditem24, iditem25, iditem26} = this.state;
         const CHAMPIONS = this.getChampions();
         const ITEMS = this.getItems();
+        console.log(ITEMS);
         return (
             <Row>
-                <Col sm={6} md={6} lg={6} className="center-container">
+                <Col sm={6} md={6} lg={6}>
+                    <Row className="center-container">
                     <SplitButton title="Campeon 1">
                         {CHAMPIONS.map(this.renderMenuItem)}
                     </SplitButton>
+                    </Row>
+                    <Row className="left-container">
+                    <SplitButton title="Item 1">
+                        {ITEMS.map(this.renderMenuItem)}
+                    </SplitButton>
+                    </Row>
+                    <Row className="left-container">
+                    <SplitButton title="Item 2">
+                        {ITEMS.map(this.renderMenuItem)}
+                    </SplitButton>
+                    </Row>
+                    <Row className="left-container">
+                    <SplitButton title="Item 3">
+                        {ITEMS.map(this.renderMenuItem)}
+                    </SplitButton>
+                    </Row>
+                    <Row className="left-container">
+                    <SplitButton title="Item 4">
+                        {ITEMS.map(this.renderMenuItem)}
+                    </SplitButton>
+                    </Row>
+                    <Row className="left-container">
+                    <SplitButton title="Item 5">
+                        {ITEMS.map(this.renderMenuItem)}
+                    </SplitButton>
+                    </Row>
+                    <Row className="left-container">
+                    <SplitButton title="Item 6">
+                        {ITEMS.map(this.renderMenuItem)}
+                    </SplitButton>
+                    </Row>
                 </Col>
-                <Col sm={6} md={6} lg={6} className="center-container">
+                <Col sm={6} md={6} lg={6}>
+                    <Row className="center-container">
                     <SplitButton title="Campeon 2">
                         {CHAMPIONS.map(this.renderMenuItem)}
                     </SplitButton>
+                    </Row>
+                    <Row className="left-container">
+                    <SplitButton title="Item 1">
+                        {ITEMS.map(this.renderMenuItem)}
+                    </SplitButton>
+                    </Row>
+                    <Row className="left-container">
+                    <SplitButton title="Item 2">
+                        {ITEMS.map(this.renderMenuItem)}
+                    </SplitButton>
+                    </Row>
+                    <Row className="left-container">
+                    <SplitButton title="Item 3">
+                        {ITEMS.map(this.renderMenuItem)}
+                    </SplitButton>
+                    </Row>
+                    <Row className="left-container">
+                    <SplitButton title="Item 4">
+                        {ITEMS.map(this.renderMenuItem)}
+                    </SplitButton>
+                    </Row>
+                    <Row className="left-container">
+                    <SplitButton title="Item 5">
+                        {ITEMS.map(this.renderMenuItem)}
+                    </SplitButton>
+                    </Row>
+                    <Row className="left-container">
+                    <SplitButton title="Item 6">
+                        {ITEMS.map(this.renderMenuItem)}
+                    </SplitButton>
+                    </Row>
                 </Col>
             </Row>
         );
