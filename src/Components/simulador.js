7a21795f8c3f3fdb0b3e187e79515084c09fd66b
Eduardo Antonio Peña Dávila <eduardo.pea45@gmail.com>
2018-05-30 12:36:28 -0400

    simulador update

--- src/Components/simulador.js
+++ src/Components/simulador.js
@@ -7,81 +7,71 @@ class Simulador extends Component {
         super(props);
 
         this.state = {
-            idchamp1: 0,
-            idchamp2: 0,
-            iditem11: 0,
-            iditem12: 0,
-            iditem13: 0,
-            iditem14: 0,
-            iditem15: 0,
-            iditem16: 0,
-            urlitem11: 0,
-            urlitem12: 0,
-            urlitem13: 0,
-            urlitem14: 0,
-            urlitem15: 0,
-            urlitem16: 0,
-            showitem11: false,
-            showitem12: false,
-            showitem13: false,
-            showitem14: false,
-            showitem15: false,
-            showitem16: false,
-            iditem21: 0,
-            iditem22: 0,
-            iditem23: 0,
-            iditem24: 0,
-            iditem25: 0,
-            iditem26: 0,
-            urlitem21: 0,
-            urlitem22: 0,
-            urlitem23: 0,
-            urlitem24: 0,
-            urlitem25: 0,
-            urlitem26: 0,
-            showitem21: false,
-            showitem22: false,
-            showitem23: false,
-            showitem24: false,
-            showitem25: false,
-            showitem16: false,
+            idchamp: [0, 0],
+            urlchamp: ["", ""],
+            boolshowchamp: [false, false],
+            iditem: [[0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0]],
+            urlitem: [["", "", "", "", "", ""],["", "", "", "", "", ""]],
+            boolshowitem: [[false, false, false, false, false, false],[false, false, false, false, false, false]],
+            champdata: [],
             itemdata: [],
+            actualItemButton: [-1,-1],
+            actualChampButton: -1
         };
 
         this.showItem = this.showItem.bind(this);
         this.closeItem = this.closeItem.bind(this);
+        this.renderMenuChamp = this.renderMenuChamp.bind(this);
         this.renderMenuItem = this.renderMenuItem.bind(this);
-        this.renderMenuItemTest = this.renderMenuItemTest.bind(this);
+        this.selectItem = this.selectItem.bind(this);
         this.getChampions = this.getChampions.bind(this);
-        this.getItems = this.getItems.bind(this);
     }
 
-    closeItem() {
-        this.setState({ showitem11: false });
+    closeItem(i, j) {
+        let items = this.state.boolshowitem;
+        items[i][j] = false;
+        this.setState({ boolshowitem: items, actualItemButton: [-1,-1] });
     }
 
-    showItem() {
-        this.setState({ showitem11: true });
+    showItem(i, j) {
+        let items = this.state.boolshowitem;
+        items[i][j] = true;
+        this.setState({ boolshowitem: items, actualItemButton: [i,j] });
     }
 
-    renderMenuItem(item, i) {
+    renderMenuChamp(champ, i) {
         return (
-            <MenuItem eventKey={item[0]}>{item[1]}</MenuItem>
+            <MenuItem eventKey={champ[0]}>{champ[1]}</MenuItem>
         );
     }
 
-    renderMenuItemTest(item, i) {
+    renderMenuItem(item) {
         const popover = (
             <Popover id={item[0]}>
-              {item[1]}
+              {item[3]}
             </Popover>
         );
+
+        const {actualItemButton} = this.state;
         
         return (
             <OverlayTrigger trigger={['hover', 'focus']} placement="left" overlay={popover}>
-            <Image src={item[2]} rounded responsive onClick={this.closeItem}/>
+            <Image src={item[2]} rounded responsive onClick={()=>{this.selectItem(item[0], item[2], actualItemButton[0], actualItemButton[1])}}/>
             </OverlayTrigger>
         );
+    }
+
+    selectItem(id, url, i, j){
+        let {iditem, urlitem} = this.state;
+        iditem[i][j] = id;
+        urlitem[i][j] = url;
+        this.setState({
+            iditem: iditem,
+            urlitem: urlitem
+        });
+        this.closeItem(i,j);
+        console.log(this.state.iditem);
+        console.log(this.state.urlitem);
     }
 
     getChampions(){
@@ -89,7 +79,7 @@ class Simulador extends Component {
         return (champs);
     }
 
-    getItems(){
+    componentDidMount(){
         fetch('http://localhost:8080/items/data/basic')
         .then(response => response.json())
         .then(result => {
@@ -100,102 +90,216 @@ class Simulador extends Component {
         .catch( error => {
             console.log("fectch error : ", error);
         });
-        
-        return this.state.itemdata;
     }
 
     render() {
-        const {idchamp1, idchamp2, iditem11, iditem12, iditem13, iditem14, iditem15, iditem16, iditem21, iditem22, iditem23, iditem24, iditem25, iditem26} = this.state;
         const CHAMPIONS = this.getChampions();
-        const ITEMS = this.getItems();
-        console.log(ITEMS);
+        const {boolshowitem,itemdata} = this.state;
         return (
             <Row>
+                {console.log(this.state.actualItemButton)}
                 <Col sm={6} md={6} lg={6}>
                     
                     <Row className="center-container">
                     <SplitButton title="Campeon 1">
-                        {CHAMPIONS.map(this.renderMenuItem)}
+                        {CHAMPIONS.map(this.renderMenuChamp)}
                     </SplitButton>
                     </Row>
-                    
+
                     <Row className="left-container">
-                        <Button bsSize="large" onClick={this.showItem}> Item 1 </Button>
+                        <Col sm={0} md={0} lg={0}>
+                            <Button bsSize="large" onClick={()=>{this.showItem(0,0)}}> Item 1 </Button>
+                        </Col>
+                        <Col sm={0} md={0} lg={0}>
+                            <Image src={this.state.urlitem} rounded responsive/>
+                        </Col>
                     </Row>
-                        <Modal show={this.state.showitem11} onHide={this.closeItem}>
+                        <Modal show={boolshowitem[0][0]} onHide={()=>{this.closeItem(0,0)}}>
                         <Modal.Header closeButton>
                             <Modal.Title>Item 1</Modal.Title>
                         </Modal.Header>
                         <Modal.Body>
-                            {ITEMS.map(this.renderMenuItemTest)}
+                            {itemdata.map(this.renderMenuItem)}
                         </Modal.Body>
                         <Modal.Footer>
-                            <Button onClick={this.closeItem}>Cerrar</Button>
+                            <Button onClick={()=>{this.closeItem(0,0)}}>Cerrar</Button>
                         </Modal.Footer>
                         </Modal>
                     
                     <Row className="left-container">
-                    <SplitButton title="Item 2">
-                        {ITEMS.map(this.renderMenuItem)}
-                    </SplitButton>
+                        <Button bsSize="large" onClick={()=>{this.showItem(0,1)}}> Item 2 </Button>
                     </Row>
+                        <Modal show={boolshowitem[0][1]} onHide={()=>{this.closeItem(0,1)}}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Item 2</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            {itemdata.map(this.renderMenuItem)}
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button onClick={()=>{this.closeItem(0,1)}}>Cerrar</Button>
+                        </Modal.Footer>
+                        </Modal>
+                    
                     <Row className="left-container">
-                    <SplitButton title="Item 3">
-                        {ITEMS.map(this.renderMenuItem)}
-                    </SplitButton>
+                        <Button bsSize="large" onClick={()=>{this.showItem(0,2)}}> Item 3 </Button>
                     </Row>
+                        <Modal show={boolshowitem[0][2]} onHide={()=>{this.closeItem(0,2)}}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Item 3</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            {itemdata.map(this.renderMenuItem)}
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button onClick={()=>{this.closeItem(0,2)}}>Cerrar</Button>
+                        </Modal.Footer>
+                        </Modal>
+                    
                     <Row className="left-container">
-                    <SplitButton title="Item 4">
-                        {ITEMS.map(this.renderMenuItem)}
-                    </SplitButton>
+                        <Button bsSize="large" onClick={()=>{this.showItem(0,3)}}> Item 4 </Button>
                     </Row>
+                        <Modal show={boolshowitem[0][3]} onHide={()=>{this.closeItem(0,3)}}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Item 4</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            {itemdata.map(this.renderMenuItem)}
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button onClick={()=>{this.closeItem(0,3)}}>Cerrar</Button>
+                        </Modal.Footer>
+                        </Modal>
+                    
                     <Row className="left-container">
-                    <SplitButton title="Item 5">
-                        {ITEMS.map(this.renderMenuItem)}
-                    </SplitButton>
+                        <Button bsSize="large" onClick={()=>{this.showItem(0,4)}}> Item 5 </Button>
                     </Row>
+                        <Modal show={boolshowitem[0][4]} onHide={()=>{this.closeItem(0,4)}}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Item 5</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            {itemdata.map(this.renderMenuItem)}
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button onClick={()=>{this.closeItem(0,4)}}>Cerrar</Button>
+                        </Modal.Footer>
+                        </Modal>
+                    
                     <Row className="left-container">
-                    <SplitButton title="Item 6">
-                        {ITEMS.map(this.renderMenuItem)}
-                    </SplitButton>
+                        <Button bsSize="large" onClick={()=>{this.showItem(0,5)}}> Item 6 </Button>
                     </Row>
+                        <Modal show={boolshowitem[0][5]} onHide={()=>{this.closeItem(0,5)}}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Item 6</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            {itemdata.map(this.renderMenuItem)}
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button onClick={()=>{this.closeItem(0,5)}}>Cerrar</Button>
+                        </Modal.Footer>
+                        </Modal>
+
                 </Col>
                 <Col sm={6} md={6} lg={6}>
+
                     <Row className="center-container">
                     <SplitButton title="Campeon 2">
-                        {CHAMPIONS.map(this.renderMenuItem)}
+                        {CHAMPIONS.map(this.renderMenuChamp)}
                     </SplitButton>
                     </Row>
+
                     <Row className="left-container">
-                    <SplitButton title="Item 1">
-                        {ITEMS.map(this.renderMenuItem)}
-                    </SplitButton>
+                        <Button bsSize="large" onClick={()=>{this.showItem(1,0)}}> Item 1 </Button>
                     </Row>
+                        <Modal show={boolshowitem[1][0]} onHide={()=>{this.closeItem(1,0)}}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Item 1</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            {itemdata.map(this.renderMenuItem)}
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button onClick={()=>{this.closeItem(1,0)}}>Cerrar</Button>
+                        </Modal.Footer>
+                        </Modal>
+                    
                     <Row className="left-container">
-                    <SplitButton title="Item 2">
-                        {ITEMS.map(this.renderMenuItem)}
-                    </SplitButton>
+                        <Button bsSize="large" onClick={()=>{this.showItem(1,1)}}> Item 2 </Button>
                     </Row>
+                        <Modal show={boolshowitem[1][1]} onHide={()=>{this.closeItem(1,1)}}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Item 2</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            {itemdata.map(this.renderMenuItem)}
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button onClick={()=>{this.closeItem(1,1)}}>Cerrar</Button>
+                        </Modal.Footer>
+                        </Modal>
+                    
                     <Row className="left-container">
-                    <SplitButton title="Item 3">
-                        {ITEMS.map(this.renderMenuItem)}
-                    </SplitButton>
+                        <Button bsSize="large" onClick={()=>{this.showItem(1,2)}}> Item 3 </Button>
                     </Row>
+                        <Modal show={boolshowitem[1][2]} onHide={()=>{this.closeItem(0,2)}}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Item 3</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            {itemdata.map(this.renderMenuItem)}
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button onClick={()=>{this.closeItem(1,2)}}>Cerrar</Button>
+                        </Modal.Footer>
+                        </Modal>
+                    
                     <Row className="left-container">
-                    <SplitButton title="Item 4">
-                        {ITEMS.map(this.renderMenuItem)}
-                    </SplitButton>
+                        <Button bsSize="large" onClick={()=>{this.showItem(1,3)}}> Item 4 </Button>
                     </Row>
+                        <Modal show={boolshowitem[1][3]} onHide={()=>{this.closeItem(0,3)}}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Item 4</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            {itemdata.map(this.renderMenuItem)}
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button onClick={()=>{this.closeItem(1,3)}}>Cerrar</Button>
+                        </Modal.Footer>
+                        </Modal>
+                    
                     <Row className="left-container">
-                    <SplitButton title="Item 5">
-                        {ITEMS.map(this.renderMenuItem)}
-                    </SplitButton>
+                        <Button bsSize="large" onClick={()=>{this.showItem(1,4)}}> Item 5 </Button>
                     </Row>
+                        <Modal show={boolshowitem[1][4]} onHide={()=>{this.closeItem(1,4)}}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Item 5</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            {itemdata.map(this.renderMenuItem)}
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button onClick={()=>{this.closeItem(1,4)}}>Cerrar</Button>
+                        </Modal.Footer>
+                        </Modal>
+                    
                     <Row className="left-container">
-                    <SplitButton title="Item 6">
-                        {ITEMS.map(this.renderMenuItem)}
-                    </SplitButton>
+                        <Button bsSize="large" onClick={()=>{this.showItem(1,5)}}> Item 6 </Button>
                     </Row>
+                        <Modal show={boolshowitem[1][5]} onHide={()=>{this.closeItem(1,5)}}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Item 6</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            {itemdata.map(this.renderMenuItem)}
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button onClick={()=>{this.closeItem(1,5)}}>Cerrar</Button>
+                        </Modal.Footer>
+                        </Modal>
+
                 </Col>
             </Row>
         );
